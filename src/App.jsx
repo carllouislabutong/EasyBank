fab7a1a9f324a4ea27b2534d43c9a5fbb706446c
Carl <carllouislabutong@gmail.com>
2023-04-22 10:56:38 +0800

    dssadas

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useEffect, useState } from "react";
+import "./App.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import Logo from "./assets/logo.svg";
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    AOS.init({ duration: 1000 });
+  });
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <header className="nav-container">
+        <nav>
+          <div>
+            <img src={Logo} alt="logo" />
+          </div>
+          <div className="nav-area">
+            <ul>
+              <li>
+                <a>Home</a>
+              </li>
+              <li>
+                <a>About</a>
+              </li>
+              <li>
+                <a>Contact</a>
+              </li>
+              <li>
+                <a>Blog</a>
+              </li>
+              <li>
+                <a>Careers</a>
+              </li>
+            </ul>
+          </div>
+          <div></div>
+        </nav>
+      </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
