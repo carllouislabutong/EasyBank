b950ad47ca28ef2ef7ecd0fd1a7fcd8fadca5da0
Carl <carllouislabutong@gmail.com>
2023-04-29 19:51:25 +0800

    firt commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,9 @@ function App() {
   useEffect(() => {
     AOS.init({ duration: 900 });
   });
+  const [openMenuLogo, setOpenMenuLogo] = useState(false);
+
+  const handleOpenMenu = () => setOpenMenuLogo(!openMenuLogo);
 
   return (
     <div>
@@ -39,6 +42,22 @@ function App() {
           <div className="nav-button">
             <button>Request Invite</button>
           </div>
+          <div className="hamburger-menu" onClick={handleOpenMenu}>
+            {openMenuLogo ? (
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="17">
+                <g fill="#2D314D" fill-rule="evenodd">
+                  <path d="M.868.661l16.97 16.97-.706.708L.162 1.369z" />
+                  <path d="M.161 17.632L17.131.662l.708.706-16.97 16.97z" />
+                </g>
+              </svg>
+            ) : (
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="11">
+                <g fill="#2D314D" fill-rule="evenodd">
+                  <path d="M0 0h24v1H0zM0 5h24v1H0zM0 10h24v1H0z" />
+                </g>
+              </svg>
+            )}
+          </div>
         </nav>
       </header>
 
@@ -52,7 +71,7 @@ function App() {
               </h1>
               <p>
                 Take you financial life online. Your Easybank account
-                <br /> Will be a one-stop-shop for spending, saving, <br />{" "}
+                <span> Will be a one-stop-shop for spending, saving,</span>{" "}
                 budgeting, investing, and much more.{" "}
               </p>
               <button>Request Invite</button>
@@ -68,7 +87,24 @@ function App() {
           </div>
         </section>
 
-        <section>flfkdfjoldshfj</section>
+        <section className="second-section">
+          <div className="main-container">
+            <div className="second-hero-section">
+              <h1>Why Choose Easybank?</h1>
+              <p>
+                We leverage Open Banking to turn your bank account into your
+                financial hub.{" "}
+                <span>Control your finances like never before.</span>
+              </p>
+            </div>
+            <div className="second-section-flex">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          </div>
+        </section>
       </main>
     </div>
   );
